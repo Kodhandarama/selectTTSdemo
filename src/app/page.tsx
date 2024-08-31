96f8d85e020c3ab551250c93af05064ed467a7b5
Kodhandarama <shreeramsureshchandra@gmail.com>
2024-08-31 18:10:03 -0500

    fixed figures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
         <img
     src="figures/Tokenizers.png"  // Replace with the path to your second image
     alt="Tokenizers"
-    style={{ width: '40%', maxWidth: '600px', height: 'auto' }}
+    style={{ width: '30%', maxWidth: '600px', height: 'auto' }}
 
   />
   <img
@@ -139,7 +139,7 @@ export default function Home() {
   
 </div>
         <div className="mt-4">
-          <p className="text-sm sm:text-md italic text-justify">
+          <p className="text-sm sm:text-md italic text-justify leading-tight">
           Proposed SelectTTS framework with the frame-selection method. In the frame selection, frames z<sub>1</sub>,z<sub>2</sub>,z<sub>3</sub>,z<sub>4</sub> are chosen through subsequence matching and frames z<sub>7</sub>, z<sub>9</sub>,z<sub>6</sub> and z<sub>10</sub> are chosen via inverse k-means sampling
           </p>
         </div>
